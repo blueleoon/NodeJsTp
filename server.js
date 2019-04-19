398720e8d069b911c3234df1f7aa329192120113
Nicolas boue <uia81777@contiwan.com>
2019-04-19 11:06:49 +0200

    falses routes and start of book model

--- server.js
+++ server.js
@@ -19,18 +19,105 @@ var port = process.env.PORT || 8080;        // set our port
 // =============================================================================
 var router = express.Router();              // get an instance of the express Router
 
+
+// middleware to use for all requests
+router.use(function (req, res, next) {
+    // do logging
+    console.log('What is happening in middleware ? ');
+    next(); // make sure we go to the next routes and don't stop here
+});
+
 // REGISTER OUR ROUTES -------------------------------
-// all of our routes will be prefixed with /api
-app.use('/api', router);
+// all of our routes will start with "/"
+app.use('/', router);
 
 // test route to make sure everything is working (accessed at GET http://localhost:8080/api)
 router.get('/test', function (req, res) {
     res.json({ message: 'welcome to my api!' });
 });
 
-// more routes for our API will happen here
+
+// on routes that end in /books
+// ----------------------------------------------------
+router.route('/books')
+
+    // create a book (accessed at POST http://localhost:8080/books)
+    .post(function (req, res) {
+
+        var book = new book();      // create a new instance of the book model
+        book.name = req.body.name;  // set the books name (comes from the request)
+
+        // save the book and check for errors
+        book.save(function (err) {
+            if (err)
+                res.send(err);
+
+            res.json({ message: 'book created!' });
+        });
+
+    })
+
+        // get all the books (accessed at POST http://localhost:8080/books)
+    .get(function (req, res) {
+        book.find(function (err, books) {
+            if (err)
+                res.send(err);
+
+            res.json(books);
+        });
+    });
+
+
+router.route('/books/:book_id')
+
+
+    // get a book with an id (accessed at POST http://localhost:8080/books)
+    .get(function (req, res) {
+        book.find(function (err, books) {
+            if (err)
+                res.send(err);
+
+            res.json(books);
+        });
+    })
+    
+
+    // update the book with this id (accessed at POST http://localhost:8080/books/:book_id)
+    .put(function (req, res) {
+
+        // use our book model to find the book we want
+        book.findById(req.params.book_id, function (err, book) {
+
+            if (err)
+                res.send(err);
+
+            book.name = req.body.name;  // update the books info
+
+            // save the book
+            book.save(function (err) {
+                if (err)
+                    res.send(err);
+
+                res.json({ message: 'book updated!' });
+            });
+
+        });
+    })
+
+    // delete the book with this id (accessed at POST http://localhost:8080/books/:book_id)
+    .delete(function (req, res) {
+        book.remove({
+            _id: req.params.book_id
+        }, function (err, book) {
+            if (err)
+                res.send(err);
+
+            res.json({ message: 'Successfully deleted' });
+        });
+    });
+
 
 // START THE SERVER
 // =============================================================================
 app.listen(port);
-console.log('Magic happens on port ' + port);
+console.log('Look how big is my port ' + port);
