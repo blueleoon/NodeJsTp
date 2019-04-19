7b57c88c89e9509147c5f30fc0f1a9cc58ca75d1
Nicolas boue <uia81777@contiwan.com>
2019-04-19 10:41:52 +0200

    books

--- server.js
+++ server.js
@@ -19,16 +19,16 @@ var port = process.env.PORT || 8080;        // set our port
 // =============================================================================
 var router = express.Router();              // get an instance of the express Router
 
-// test route to make sure everything is working (accessed at GET http://localhost:8080/api)
-router.get('/', function (req, res) {
-    res.json({ message: 'hooray! welcome to my api!' });
-});
-
-// more routes for our API will happen here
-
 // REGISTER OUR ROUTES -------------------------------
 // all of our routes will be prefixed with /api
 app.use('/api', router);
+
+// test route to make sure everything is working (accessed at GET http://localhost:8080/api)
+router.get('/test', function (req, res) {
+    res.json({ message: 'welcome to my api!' });
+});
+
+// more routes for our API will happen here
 
 // START THE SERVER
 // =============================================================================
